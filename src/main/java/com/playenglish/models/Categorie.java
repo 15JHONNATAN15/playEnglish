2c5eb6099ead0e8007f7bbfdc34aa5b1031c03cb
candvargas <candvargas@poligran.edu.co>
2022-11-22 18:25:33 -0500

    add methods to models

--- src/main/java/com/playenglish/models/Categorie.java
+++ src/main/java/com/playenglish/models/Categorie.java
@@ -1,8 +1,12 @@
 package com.playenglish.models;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Categorie {
     private Integer id;
     private String name;
--- src/main/java/com/playenglish/models/Word.java
+++ src/main/java/com/playenglish/models/Word.java
@@ -1,11 +1,23 @@
 package com.playenglish.models;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Word {
     private Integer id;
-    private String nameSpanish;
-    private String nameEnglish;
+    private String wordSpanish;
+    private String wordEnglish;
     private Categorie categorie;
+
+    public boolean validateWordSpanish(String input) {
+        return this.wordSpanish.contains(input);
+    }
+
+    public boolean validateWordEnglish(String input) {
+        return this.wordEnglish.contains(input);
+    }
 }
